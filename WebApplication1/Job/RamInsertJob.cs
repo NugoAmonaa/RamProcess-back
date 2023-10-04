28bc99f18498dedb2e2e2de7f8a4aecfc33a4540
kiu_amonashvili.nugzari <amonashvili.nugzari@kiu.edu.ge>
2023-10-04 23:41:08 +0400

    add quartz

---
+++ WebApplication1/Job/RamInsertJob.cs
@@ -0,0 +1,42 @@
+﻿using Quartz;
+using RamProcessingTool.Entity;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WebApplication1.Repository;
+
+public class RamInsertJob : IJob
+{
+    private readonly IRamEntityRepository _ramEntityRepository;
+    
+
+    public RamInsertJob(IRamEntityRepository ramEntityRepository)
+    {
+        _ramEntityRepository = ramEntityRepository;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+
+            Process[] processes = Process.GetProcesses();
+            long totalMemoryUsageKB = 0;
+
+            foreach (var process in processes)
+            {
+                totalMemoryUsageKB += process.PrivateMemorySize64 / 1024;
+            }
+
+            DateTime date = DateTime.Now;
+            RamEntity ramEntity = new RamEntity()
+            {
+                Name = $"{nameof(RamEntity)}",
+                Date = date,
+                Size = totalMemoryUsageKB / 1024
+            };
+
+            await _ramEntityRepository.Insert(ramEntity);
+            await _ramEntityRepository.SaveChanges();
+
+       
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,9 @@
+using Quartz.Impl;
+using Quartz.Spi;
+using Quartz;
 using VacationScaffold.RepositoryImplementation;
 using WebApplication1.Repository;
+using Quartz.Simpl;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +17,26 @@ builder.Services.AddDbContext<ProjectContext>();
 
 builder.Services.AddScoped<IRamEntityRepository, RamEntityRepository>();
 
+builder.Services.AddQuartz(options =>
+{
+    options.UseMicrosoftDependencyInjectionJobFactory();
+    options.AddJob<RamInsertJob>(JobKey.Create(nameof(RamInsertJob)))
+    .AddTrigger(trigger =>
+    {
+        trigger.ForJob(nameof(RamInsertJob))
+         //.StartNow()
+            .WithSimpleSchedule(x => x
+                .WithIntervalInSeconds(1)
+                .RepeatForever());
+
+    });
+});
+
+builder.Services.AddQuartzHostedService();
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -29,7 +51,5 @@ app.UseCors(x => x
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
